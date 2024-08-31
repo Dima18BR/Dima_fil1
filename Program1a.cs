3542fad357a29f74797d70fada37458e4b13a2eb
Dima18BR <bredikhin18_19dabrahma@outlook.com>
2024-08-31 16:12:05 +1000

    Update Program1a.cs

--- Program1a.cs
+++ Program1a.cs
@@ -1,8 +1,11 @@
 using System;
 namespace Program1a;
-{
+{ 
+  class Program
+  {
   public static void main(string [args]);
     {
        System.Conole.WeiteLine($"Первая страница");
     }
+  }
 }
